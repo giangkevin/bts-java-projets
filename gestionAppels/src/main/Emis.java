c27574cc82eb8ede94fc854bb13e675c7c9bab8e
giangkevin <149978467+giangkevin@users.noreply.github.com>
2023-12-28 18:12:00 +0100

    commit gestionAppels

---
+++ gestionAppels/src/main/Emis.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.time.LocalDate;
+
+
+public class Emis extends Appel{
+	public Emis(String numero, LocalDate date, int duree) {
+		super(numero, date, duree);
+		this.cout = 1;
+		 
+	}
+	
+
+	public String toString() {
+		return super.toString() + "et cout total = " + this.getCout();
+	}
+}
---
+++ gestionAppels/src/main/Recu.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.time.LocalDate;
+
+
+public class Recu extends Appel{
+
+	public Recu(String numero, LocalDate date, int duree) {
+		super(numero, date, duree);
+		// TODO Auto-generated constructor stub
+	}
+
+}
